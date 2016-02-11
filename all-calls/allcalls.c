b4e1fd1811dc586e5d84c1a98ae781e1fb410cbd
Mohamad Gebai <mohamad.gebai@gmail.com>
2016-02-11 09:54:53 -0500

    Fixing indentation issues
    Signed-off-by: Mohamad Gebai <mohamad.gebai@gmail.com>

--- all-calls/allcalls.c
+++ all-calls/allcalls.c
@@ -5,184 +5,170 @@
 #include <popt.h>
 #include <pthread.h>
 #include <time.h>
+#include <stdlib.h>
 
 #define BENCHMOD_NAME "/proc/benchmod"
 
 struct popt_args {
-	int clear;
-	int loop;
-	int nthreads;
-	char *outfile;
-	char *tracer;
+    int clear;
+    int loop;
+    int nthreads;
+    char *outfile;
+    char *tracer;
 };
 
 struct popt_args popt_args;
 
-
 struct poptOption options[] = {
-	{
-		NULL,
-		'n',
-		POPT_ARG_INT | POPT_ARGFLAG_OPTIONAL,
-		&popt_args.loop,
-		NULL,
-		"Number of times to run the system call",
-		"Desc"
-	},
-	{
-		"output",
-		'o',
-		POPT_ARG_STRING | POPT_ARGFLAG_OPTIONAL,
-		&popt_args.outfile,
-		NULL,
-		"File to append to",
-		"Desc file"
-	},
-	{
-		"tracer",
-		't',
-		POPT_ARG_STRING | POPT_ARGFLAG_OPTIONAL,
-		&popt_args.tracer,
-		NULL,
-		"Tracer",
-		"tracer"
-	},
-	{
-		"threads",
-		'p',
-		POPT_ARG_INT | POPT_ARGFLAG_OPTIONAL,
-		&popt_args.nthreads,
-		NULL,
-		"Number of threads",
-		"Nthreads"
-	},
-	{
-		"clear",
-		'c',
-		POPT_ARG_NONE | POPT_ARGFLAG_OPTIONAL,
-		&popt_args.clear,
-		NULL,
-		"Clear file",
-		"clear"
-	},
-	POPT_AUTOHELP
+    {
+        NULL, 'n',
+        POPT_ARG_INT | POPT_ARGFLAG_OPTIONAL,
+        &popt_args.loop, 0,
+        "Number of times to run the system call", "Desc"
+    },
+    {
+        "output", 'o',
+        POPT_ARG_STRING | POPT_ARGFLAG_OPTIONAL,
+        &popt_args.outfile, 0,
+        "File to append to", "Desc file"
+    },
+    {
+        "tracer", 't',
+        POPT_ARG_STRING | POPT_ARGFLAG_OPTIONAL,
+        &popt_args.tracer, 0,
+        "Tracer", "tracer"
+    },
+    {
+        "threads", 'p',
+        POPT_ARG_INT | POPT_ARGFLAG_OPTIONAL,
+        &popt_args.nthreads, 0, "Nthreads"
+    },
+    {
+        "clear", 'c',
+        POPT_ARG_NONE | POPT_ARGFLAG_OPTIONAL,
+        &popt_args.clear, 0,
+        "Clear file", "clear"
+    },
+    POPT_AUTOHELP
 };
 
+pthread_barrier_t barrier;
 
 struct ioctl_args {
-	int fd;
-	int ntimes;
+    int fd;
+    int ntimes;
 };
 
 struct timespec ts_diff(struct timespec start, struct timespec end)
 {
-	struct timespec temp;
-	if ((end.tv_nsec - start.tv_nsec) < 0) {
-		temp.tv_sec = end.tv_sec-start.tv_sec-1;
-		temp.tv_nsec = 1000000000 + end.tv_nsec-start.tv_nsec;
-	} else {
-		temp.tv_sec = end.tv_sec-start.tv_sec;
-		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-	}
-	return temp;
+    struct timespec temp;
+    if ((end.tv_nsec - start.tv_nsec) < 0) {
+        temp.tv_sec = end.tv_sec-start.tv_sec-1;
+        temp.tv_nsec = 1000000000 + end.tv_nsec-start.tv_nsec;
+    } else {
+        temp.tv_sec = end.tv_sec-start.tv_sec;
+        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+    return temp;
 }
 
 static void parse_args(int argc, char **argv, poptContext *pc)
 {
-	int val;
+    int val;
 
-	*pc = poptGetContext(NULL, argc, (const char **)argv, options, 0);
+    *pc = poptGetContext(NULL, argc, (const char **)argv, options, 0);
 
-	if (argc < 2) {
-		poptPrintUsage(*pc, stderr, 0);
-		return;
-	}
+    if (argc < 2) {
+        poptPrintUsage(*pc, stderr, 0);
+        return;
+    }
 
-	while ((val = poptGetNextOpt(*pc)) >= 0) {
-		printf("poptGetNextOpt returned val %d\n", val);
-	}
+    while ((val = poptGetNextOpt(*pc)) >= 0) {
+        printf("poptGetNextOpt returned val %d\n", val);
+    }
 }
 
 static void dump(char *outfile, struct timespec timespan)
 {
-	FILE *file;
-	unsigned long latency, totaltime;
+    FILE *file;
+    unsigned long totaltime;
 
-	totaltime = timespan.tv_sec * 1000000000 + timespan.tv_nsec;
-//	latency = totaltime / popt_args.loop;
+    totaltime = timespan.tv_sec * 1000000000 + timespan.tv_nsec;
 
-	file = fopen(outfile, popt_args.clear ? "w+" : "a+");
-	if(popt_args.clear) {
-		fprintf(file, "tracer,total time,number of events,number of threads\n");
-	}
-	fprintf(file, "%s,%ld,%d,%d\n", popt_args.tracer, totaltime, popt_args.loop,
-			popt_args.nthreads);
-	fprintf(stdout, "%ld\n", timespan.tv_nsec);
+    file = fopen(outfile, popt_args.clear ? "w+" : "a+");
 
-	close(file);
-}
+    if(!file) {
+        fprintf(stderr, "Error opening file %s\n", outfile);
+        return;
+    }
 
-static inline void do_ioctl(int fd, unsigned long request)
-{
+    if(popt_args.clear) {
+        fprintf(file, "number of threads,total time,number of events\n");
+    }
+    fprintf(file, "%d,%ld,%d\n", popt_args.nthreads, totaltime,
+            popt_args.loop);
+    fprintf(stdout, "%ld\n", timespan.tv_nsec);
+
+    fclose(file);
 }
 
 void *do_work(void *args)
 {
-	struct ioctl_args *arg = args;
-	int i, uid;
-	struct timespec ts_temp;
+    struct ioctl_args *arg = args;
+    int i, fd;
 
-//	int fd = open(BENCHMOD_NAME, O_RDONLY);
+    fd = open(BENCHMOD_NAME, O_RDONLY);
 
-	for(i = 0; i < arg->ntimes; i++) {
-//		clock_gettime(CLOCK_MONOTONIC_RAW, &ts_temp);
-        uid = getuid();
-//        ioctl(fd, 0);
-//		read(arg->fd, "a", 1);
-	}
-//	close(fd);
+    pthread_barrier_wait(&barrier);
+    // This ioctl will write a tracepoint in a tight loop
+    ioctl(fd, 0, arg->ntimes);
+    close(fd);
 }
 
 int main(int argc, char **argv)
 {
-	popt_args.outfile = "default";
-	popt_args.clear = 0;
-	popt_args.tracer = "none";
-	popt_args.nthreads = 1;
-	popt_args.loop = 10;
+    popt_args.outfile = "none.out";
+    popt_args.clear = 0;
+    popt_args.tracer = "none";
+    popt_args.nthreads = 1;
+    popt_args.loop = 1;
 
-	int i, fd, ret = 0;
-	struct timespec ts_beg, ts_end, ts_elapsed;
-	struct ioctl_args ioctl_arg;
-	poptContext pc;
-	pthread_t *threads;
+    int i, fd, ret = 0;
+    struct timespec ts_beg, ts_end, ts_elapsed;
+    struct ioctl_args ioctl_arg;
+    poptContext pc;
+    pthread_t *threads;
 
-	parse_args(argc, argv, &pc);
-	printf("nthreads = %d, loop = %d\n", popt_args.nthreads, popt_args.loop);
+    parse_args(argc, argv, &pc);
+    printf("nthreads = %d, loop = %d\n", popt_args.nthreads, popt_args.loop);
 
-	threads = (pthread_t*)malloc(popt_args.nthreads * sizeof(pthread_t));
+    threads = (pthread_t*)malloc(popt_args.nthreads * sizeof(pthread_t));
 
-	if(fd == -1) {
-		printf("Error opening %s\n", BENCHMOD_NAME);
-		return 1;
-	}
+    if(fd == -1) {
+        printf("Error opening %s\n", BENCHMOD_NAME);
+        return 1;
+    }
 
-	ioctl_arg.fd = fd;
-	ioctl_arg.ntimes = popt_args.loop / popt_args.nthreads;
+    ioctl_arg.fd = fd;
+    ioctl_arg.ntimes = popt_args.loop / popt_args.nthreads;
 
-	for(i = 0; i < popt_args.nthreads; i++) {
-		pthread_create(&threads[i], NULL, do_work, (void*)&ioctl_arg);
-	}
+    pthread_barrier_init(&barrier, NULL, popt_args.nthreads + 1);
+    for(i = 0; i < popt_args.nthreads; i++) {
+        pthread_create(&threads[i], NULL, do_work, (void*)&ioctl_arg);
+    }
 
-	clock_gettime(CLOCK_MONOTONIC, &ts_beg);
-	for(i = 0; i < popt_args.nthreads; i++) {
-		pthread_join(threads[i], NULL);
-	}
-	clock_gettime(CLOCK_MONOTONIC, &ts_end);
+    pthread_barrier_wait(&barrier);
+    clock_gettime(CLOCK_MONOTONIC, &ts_beg);
+    for(i = 0; i < popt_args.nthreads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    clock_gettime(CLOCK_MONOTONIC, &ts_end);
 
-	ts_elapsed = ts_diff(ts_beg, ts_end);
+    pthread_barrier_destroy(&barrier);
 
-	dump(popt_args.outfile, ts_elapsed);
+    ts_elapsed = ts_diff(ts_beg, ts_end);
 
-	return ret;
+    dump(popt_args.outfile, ts_elapsed);
+
+    return ret;
 }
