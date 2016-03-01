756ebe05e46ec19a8556512d692e578fe04486b3
Mohamad Gebai <mohamad.gebai@gmail.com>
2016-03-01 13:25:02 -0500

    Copy /tmp/out.csv instead of reading /proc/benchmod
    Signed-off-by: Mohamad Gebai <mohamad.gebai@gmail.com>

--- all-calls/allcalls.c
+++ all-calls/allcalls.c
@@ -75,6 +75,66 @@ struct ioctl_args {
     int id;
 };
 
+int copy(const char *from, const char *to)
+{
+    int fd_to, fd_from;
+    char buf[4096];
+    ssize_t nread;
+    int saved_errno;
+
+    fd_from = open(from, O_RDONLY);
+    if (fd_from < 0)
+        return -1;
+
+    fd_to = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (fd_to < 0)
+        goto out_error;
+
+    while (nread = read(fd_from, buf, sizeof buf), nread > 0)
+    {
+        char *out_ptr = buf;
+        ssize_t nwritten;
+
+        do {
+            nwritten = write(fd_to, out_ptr, nread);
+
+            if (nwritten >= 0)
+            {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+            else if (errno != EINTR)
+            {
+                goto out_error;
+            }
+        } while (nread > 0);
+    }
+
+    if (nread == 0)
+    {
+        if (close(fd_to) < 0)
+        {
+            fd_to = -1;
+            goto out_error;
+        }
+        close(fd_from);
+
+        /* Success! */
+        return 0;
+    }
+
+  out_error:
+    saved_errno = errno;
+    printf("Error during copy, error = %s\n", strerror(errno));
+
+    close(fd_from);
+    if (fd_to >= 0)
+        close(fd_to);
+
+    errno = saved_errno;
+    return -1;
+}
+
 static inline int do_ioctl_benchmark(int fd, struct benchmod_arg *args)
 {
     return ioctl(fd, _IO('m', 0), args);
@@ -149,16 +209,18 @@ static void dump(char *tracer, struct timespec timespan)
     strcat(outfile, tmp_str);
     sprintf(tmp_str, "_%dprocess", popt_args.nthreads);
     strcat(outfile, tmp_str);
-    file = fopen(strcat(outfile, ".hist"), "w+");
-    fd = open(BENCHMOD_NAME, O_RDONLY);
+    strcat(outfile, ".hist");
+    copy("/tmp/out.csv", outfile);
+//    file = fopen(outfile, "w+");
+//    fd = open(BENCHMOD_NAME, O_RDONLY);
 
-    /* Dump the content of /proc/benchmod */
-    while(read(fd, huge, 1048576) > 0) {
-        fprintf(file, "%s", huge);
-    }
+//    /* Dump the content of /proc/benchmod */
+//    while(read(fd, huge, 1048576) > 0) {
+//        fprintf(file, "%s", huge);
+//    }
 
-    close(fd);
-    fclose(file);
+//    close(fd);
+//    fclose(file);
 }
 
 void *do_work(void *args)
