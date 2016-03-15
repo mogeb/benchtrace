36e019ae738cd45c26aa50f747af8f18c9b7be53
Mohamad Gebai <mohamad.gebai@gmail.com>
2016-03-15 17:56:11 -0400

    Added default values for trace-client
    Signed-off-by: Mohamad Gebai <mohamad.gebai@gmail.com>

--- trace-client/trace-client.py
+++ trace-client/trace-client.py
@@ -8,7 +8,7 @@ def error_opt(msg):
 
 
 def main(args):
-    longopts = ["tracer=", "workload=", "size=", "process=", "loop="]
+    longopts = ["tracer=", "workload=", "size=", "process=", "loop=", "subbuf-size", "num-subbuf="]
     try:
         optlist, args = getopt.getopt(args[1:], "t:w:s:p:n:zb:", longopts)
     except getopt.GetoptError as err:
@@ -21,6 +21,9 @@ def main(args):
     # buf_size_kb = 524288
     buf_size_kb = 262144
     do_work = False
+    num_sub_buf_kb = 4
+    buf_sizes_kb = [131072]
+    tp_sizes = [4]
 
     """
     Parse arguments
@@ -30,20 +33,23 @@ def main(args):
             tracer_args = arg.split(",")
         elif opt == "--workload" or opt == "-w":
             workload_args = arg.split(",")
-        elif opt == "--size" or opt == "-s":
+        elif opt == "--payload-size" or opt == "-s":
             tp_sizes = arg.split(",")
         elif opt == "--process" or opt == "-p":
-            nprocess = arg.split(",")
+            nprocesses = arg.split(",")
         elif opt == "--loop" or opt == "-n":
             loop = arg
         elif opt == "-z":
             do_work = True
-        elif opt == "-b":
-            buf_size_kb = arg
+        elif opt == "--subbuf-size" or opt == "-b":
+            buf_sizes_kb = arg.split(",")
+        elif opt == "--num-subbuf":
+            num_sub_buf_kb = arg
 
-    if int(buf_size_kb) > 262144:
-        print("OVERRIDING BUFFER SIZE TO 262144")
-        buf_size_kb = str(262144)
+    for i in range(0, len(buf_sizes_kb)):
+        if int(buf_sizes_kb[i]) > 262144:
+            print("OVERRIDING BUFFER SIZE TO 262144")
+            buf_size_kb[buf_sizes_kb] = str(262144)
 
 
     for workload_arg in workload_args:
@@ -52,8 +58,9 @@ def main(args):
         except ImportError as err:
             print('Import error: ' + str(err))
 
-        args = { 'buf_size_kb' : buf_size_kb, 'tp_sizes': tp_sizes,
-                 'nprocess' : nprocess, 'loop' : loop }
+        args = { 'buf_sizes_kb' : buf_sizes_kb, 'tp_sizes': tp_sizes,
+                 'nprocesses' : nprocesses, 'loop' : loop, 'tracers': tracer_args,
+                 'num_subbuf': num_sub_buf_kb}
         if do_work:
             for tracer_arg in tracer_args:
                 try:
