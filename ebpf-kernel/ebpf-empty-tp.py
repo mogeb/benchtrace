89442d17a31792ddaacd2d905ae791f8a8e33e47
Mohamad Gebai <mohamad.gebai@gmail.com>
2016-08-30 10:49:20 -0400

    Milestone article
    Signed-off-by: Mohamad Gebai <mohamad.gebai@gmail.com>

--- ebpf-kernel/ebpf-empty-tp.py
+++ ebpf-kernel/ebpf-empty-tp.py
@@ -17,30 +17,20 @@ class Event(ct.Structure):
 
 def print_event(cpu, data, size):
     event = ct.cast(data, ct.POINTER(Event)).contents
-#    print(event.ts)
 
 if __name__ == "__main__":
     b = BPF(src_file="ebpf-interface.c")
     b.attach_kprobe(event="tp_4b", fn_name="connect_tp")
     b["trace"].open_perf_buffer(print_event)
-#    b.attach_kprobe(event="update_wall_time", fn_name="connect_tp")
 
     args = sys.argv
     try:
-        optlist, args = getopt.getopt(args[1:], "p:n:")
+        optlist, args = getopt.getopt(args[1:], "c:")
     except getopt.GetoptError as err:
         error_opt(str(err))
 
-    loops = 10
-    threads = 1
-
     for opt, arg in optlist:
-        if opt == "-n":
-            loops = arg
-        elif opt == "-p":
-            threads = arg
+        if opt == '-c':
+            cmd = arg
 
-    call(('/home/mogeb/git/benchtrace/all-calls/bin/allcalls -n %s -p %s') % (loops, threads), shell=True)
-
-#    while 1:
-#        b.kprobe_poll()
+    call(cmd, shell=True)
