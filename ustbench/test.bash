9683ddeef5f0ab66c21aead923cef07352e17bf4
Mohamad Gebai <mohamad.gebai@gmail.com>
2016-10-31 23:00:53 -0400

    Remove test.bash
    Signed-off-by: Mohamad Gebai <mohamad.gebai@gmail.com>

--- ustbench/test.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-lttng create session-test -o .
-lttng enable-channel chan0 --subbuf-size 8k --num-subbuf 2 -u
-lttng enable-event -u -c chan0 ustbench:bench_tp_4b
-lttng start
-/home/mogeb/git/benchtrace/ustbench/bin/ustbench -n 100 -p 1 -t lttng-ust
-lttng stop
-lttng destroy
